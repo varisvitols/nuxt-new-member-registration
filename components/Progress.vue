790640e6ac1e3ad42606df8c9def6baed64a9b8c
Varis Vitols <varisvitols@gmail.com>
2021-04-08 12:34:10 +0300

    Progress component created

---
+++ components/Progress.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="progress">
+    <div class="progress__item  progress__item--done">
+      <div class="progress__item-number">1</div>
+      <div class="progress__item-text">Personal info</div>
+    </div>
+    <div class="progress__item  progress__item--active">
+      <div class="progress__item-number">2</div>
+      <div class="progress__item-text">Membership</div>
+    </div>
+    <div class="progress__item">
+      <div class="progress__item-number">3</div>
+      <div class="progress__item-text">Overview</div>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+// import { Component, Vue } from 'vue-property-decorator'
+
+// @Component
+// export default class Progress extends Vue {}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <Progress />
     <Nuxt />
   </div>
 </template>
